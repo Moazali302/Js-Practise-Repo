b71e9afc18bbaa524c6759c7a0b9fbe2b42b4a43
Moazali302 <moazj048@gmail.com>
2025-08-10 00:08:03 +0500

    Js word count

--- practise.js
+++ practise.js
@@ -49,3 +49,69 @@ console.log(updatename);
 let passedstudent=updatename.filter((student)=>student.marks>=50)
 console.log("congragulations!passed student name is :")
 console.log(passedstudent);
+  
+
+
+let btn=document.getElementById("highlight-btn")
+ btn.addEventListener("click",function(){
+
+
+  let allitems=document.querySelectorAll("#task-list li")
+   allitems.forEach(items => {
+     items.style.background="skyBlue";
+   });
+
+    firstitems=document.querySelector("#task-list li:first-child")
+    firstitems.style.fontWeight="bold";
+
+    lastitem=document.querySelector("#task-list li:last-child")
+
+   lastitem.style.color="red";
+   })
+  let input=document.getElementById("user")
+  let div=document.getElementById("greet")
+ let btn1=document.getElementById("Show Greeting")
+ btn1.addEventListener("click",function(){
+    const name=document.getElementById("user").value;
+    document.getElementById("greet").innerText="welcome  " +name+"!";
+ })
+
+ let p=document.getElementById("changeText")
+ let btn3=document.getElementById("change")
+ btn3.addEventListener("click",function(){
+    p.innerText="Hi im changed";
+ })
+
+  let div1=document.getElementsByClassName("random")[0];
+  let butn=document.getElementById("randomcolor");
+  butn.addEventListener("click",function() {
+    let colour=['red','blue','yellow','skyBlue'];
+    let randomindex=Math.floor(Math.random()*colour.length);
+    div1.style.backgroundColor=colour[randomindex];
+  })
+
+  
+// Task 3 (Practice Question):
+// Ek <textarea> banao aur ek <p> tag jisme likha ho "Characters: 0".
+// Jab user type kare, <p> me total typed characters ka count real-time update hota rahe.
+
+// Bonus: Agar characters 50 se zyada ho jayein to text ka color red ho jaye.
+
+
+
+let head=document.getElementById("textCount")
+let textarea=document.getElementById("textArea")
+textarea.addEventListener("input",function(){
+  let  res=textarea.value.length;
+   head.innerText="characater:"+res;
+  if(res>=50){
+    head.style.backgroundColor="red";
+  }
+  else{
+    if(res<=50){
+      head.style.backgroundColor="white"
+    }
+    head.style.backgroundColor="white"
+  }
+  }
+)
